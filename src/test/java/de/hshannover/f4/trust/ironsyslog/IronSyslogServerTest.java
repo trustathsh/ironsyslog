87bc7dda6bf54be2f1d00ce1d70cdf077f67ebe7
Bastian Hellmann <bahellma@googlemail.com>
2015-04-08 14:52:28 +0200

    [fixed] wrong IP address used in tests

--- src/test/java/de/hshannover/f4/trust/ironsyslog/IronSyslogServerTest.java
+++ src/test/java/de/hshannover/f4/trust/ironsyslog/IronSyslogServerTest.java
@@ -7,17 +7,17 @@
  *    | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
  *    |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
  *                             \____/
- * 
+ *
  * =====================================================
- * 
+ *
  * Hochschule Hannover
  * (University of Applied Sciences and Arts, Hannover)
  * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
- * 
+ *
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de/
- * 
+ *
  * This file is part of ironsyslog, version 0.0.3,
  * implemented by the Trust@HsH research group at the Hochschule Hannover.
  * %%
@@ -26,9 +26,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -54,71 +54,71 @@ import de.hshannover.f4.trust.ironsyslog.syslog.IronSyslogServer;
 
 /**
  * Test class for the setup of the syslog server.
- * 
+ *
  * @author Leonard Renners
- * 
+ *
  */
 public class IronSyslogServerTest {
 
-    private IronSyslogServer ironsyslog;
-    private static final String HOST_IP = "192.168.1.53";
+	private IronSyslogServer ironsyslog;
+	private static final String HOST_IP = "127.0.0.1";
 
-    /**
-     * Sets up the test environment.
-     */
-    @Before
-    public void setUp() {
-        System.setProperty("log4j.defaultInitOverride", "false");
-        System.setProperty("log4j.configuration", "log4j.properties");
-        ironsyslog = new IronSyslogServer();
-    }
+	/**
+	 * Sets up the test environment.
+	 */
+	@Before
+	public void setUp() {
+		System.setProperty("log4j.defaultInitOverride", "false");
+		System.setProperty("log4j.configuration", "log4j.properties");
+		ironsyslog = new IronSyslogServer();
+	}
 
-    /**
-     * Tests the server setup.
-     */
-    @Test
-    public void testServerSetup() {
-        assertFalse(ironsyslog.isServerRunning());
-        assertFalse(ironsyslog.isServerRunning("udp"));
+	/**
+	 * Tests the server setup.
+	 */
+	@Test
+	public void testServerSetup() {
+		assertFalse(ironsyslog.isServerRunning());
+		assertFalse(ironsyslog.isServerRunning("udp"));
 
-        ironsyslog.setupServer(HOST_IP, 9999, "udp");
-        assertTrue(ironsyslog.isServerRunning());
-        assertTrue(ironsyslog.isServerRunning("udp"));
-        assertFalse(ironsyslog.isServerRunning("tcp"));
+		ironsyslog.setupServer(HOST_IP, 9999, "udp");
+		assertTrue(ironsyslog.isServerRunning());
+		assertTrue(ironsyslog.isServerRunning("udp"));
+		assertFalse(ironsyslog.isServerRunning("tcp"));
 
-        ironsyslog.setupServer(HOST_IP, 63999, "tcp");
-        assertEquals(63999, ironsyslog.getPortForProtocol("tcp"));
-        assertTrue(ironsyslog.isServerRunning());
-        assertTrue(ironsyslog.isServerRunning("tcp"));
+		ironsyslog.setupServer(HOST_IP, 10999, "tcp");
+		assertEquals(10999, ironsyslog.getPortForProtocol("tcp"));
+		assertTrue(ironsyslog.isServerRunning());
+		assertTrue(ironsyslog.isServerRunning("tcp"));
 
-        ironsyslog.shutdownServer("udp");
-        assertFalse(ironsyslog.isServerRunning("udp"));
-        assertTrue(ironsyslog.isServerRunning());
+		ironsyslog.shutdownServer("udp");
+		assertFalse(ironsyslog.isServerRunning("udp"));
+		assertTrue(ironsyslog.isServerRunning());
 
-        ironsyslog.shutdownServer("tcp");
-        assertFalse(ironsyslog.isServerRunning("tcp"));
-        assertFalse(ironsyslog.isServerRunning());
+		ironsyslog.shutdownServer("tcp");
+		assertFalse(ironsyslog.isServerRunning("tcp"));
+		assertFalse(ironsyslog.isServerRunning());
 
-        ironsyslog.setupSslTcpServer(HOST_IP, 63998);
-        assertEquals(63998, ironsyslog.getPortForProtocol("ssltcp"));
-        assertTrue(ironsyslog.isServerRunning());
-        assertTrue(ironsyslog.isServerRunning("ssltcp"));
+		ironsyslog.setupSslTcpServer(HOST_IP, 63998);
+		assertEquals(63998, ironsyslog.getPortForProtocol("ssltcp"));
+		assertTrue(ironsyslog.isServerRunning());
+		assertTrue(ironsyslog.isServerRunning("ssltcp"));
 
-        ironsyslog.shutdownServer("ssltcp");
-        assertFalse(ironsyslog.isServerRunning("ssltcp"));
-        assertFalse(ironsyslog.isServerRunning());
+		ironsyslog.shutdownServer("ssltcp");
+		assertFalse(ironsyslog.isServerRunning("ssltcp"));
+		assertFalse(ironsyslog.isServerRunning());
 
-        Syslog.getInstance("udp").getConfig().setHost(HOST_IP);
-        Syslog.getInstance("udp").getConfig().setPort(514);
-        Syslog.getInstance("udp").log(SyslogLevel.INFO, "test");
-    }
+		Syslog.getInstance("udp").getConfig().setHost(HOST_IP);
+		Syslog.getInstance("udp").getConfig().setPort(514);
+		Syslog.getInstance("udp").log(SyslogLevel.INFO, "test");
+	}
 
-    /**
-     * Tears down the test environment.
-     */
-    @After
-    public void tearDown() {
-        ironsyslog.shutdown();
-    }
+	/**
+	 * Tears down the test environment.
+	 */
+	@After
+	public void tearDown() {
+		ironsyslog.shutdown();
+	}
 
 }
